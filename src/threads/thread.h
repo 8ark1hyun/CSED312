2efc92a88579ea9aa5ed34f087a14a264eb335c6
dalmutimelange <dkim011006@postech.ac.kr>
2024-09-21 03:49:51 +0900

    ADD: thread_awake thread_sleep header

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -141,4 +141,8 @@ void thread_set_nice (int);
 int thread_get_recent_cpu (void);
 int thread_get_load_avg (void);
 
+// alarm clock - pintos 1
+void thread_sleep(int64_t ticks);
+void thread_awake(int64_t ticks);
+
 #endif /* threads/thread.h */
