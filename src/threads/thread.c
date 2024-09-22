a09577d656e5a7a31ef5b0f95839c8d61cc0ebd2
dalmutimelange <dkim011006@postech.ac.kr>
2024-09-23 00:31:16 +0900

    ADD: initializing

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -497,6 +497,11 @@ init_thread (struct thread *t, const char *name, int priority)
   old_level = intr_disable ();
   list_push_back (&all_list, &t->allelem);
   intr_set_level (old_level);
+
+  // priority scheduling - pintos 1
+  t -> original_priority = priority;
+  t -> waiting_lock = NULL;
+  list_init(&t -> donations);
 }
 
 /* Allocates a SIZE-byte frame at the top of thread T's stack and
