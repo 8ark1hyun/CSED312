160b6bccb67ea633ff732ae819905307d573b23a
8ark1hyun <kihyun@postech.ac.kr>
2024-10-16 00:52:10 +0900

    ADD: System Calls Skeleton

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -18,3 +18,83 @@ syscall_handler (struct intr_frame *f UNUSED)
   printf ("system call!\n");
   thread_exit ();
 }
+
+// System Calls - pintos 2
+void
+halt (void)
+{
+  shutdown_power_off ();
+}
+
+void
+exit (int status)
+{
+
+}
+
+pid_t
+exec (const char *cmd_line)
+{
+
+}
+
+int
+wait (pid_t pid)
+{
+
+}
+
+bool
+create (const char *file, unsigned initial_size)
+{
+
+}
+
+bool
+remove (const char *file)
+{
+
+}
+
+int
+open (const char *file)
+{
+
+}
+
+int
+filesize (int fd)
+{
+
+}
+
+int
+read (int fd, void *buffer, unsigned size)
+{
+
+}
+
+int
+write (int fd, const void *buffer, unsigned size)
+{
+
+}
+
+void
+seek (int fd, unsigned position)
+{
+
+}
+
+unsigned
+tell (int fd)
+{
+
+}
+
+void
+close (int fd)
+{
+
+}
+// end
